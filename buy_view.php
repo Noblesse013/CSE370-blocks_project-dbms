6440158ee37a5f2034c23d5b471ea52c54c36f3b
Sadab14 <sm.sadab.shakib@g.bracu.ac.bd>
2025-01-06 17:09:57 +0600

    changed buy_view.php and create_order.php and renamed css/buy_styles.css to css/buy_style.css

--- buy_view.php
+++ buy_view.php
@@ -12,7 +12,7 @@ $result = $conn->query($sql);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Buyer View</title>
-    <link rel="stylesheet" href="css/buy_styles.css">
+    <link rel="stylesheet" href="css/buy_style.css">
 </head>
 <body>
     <header>
