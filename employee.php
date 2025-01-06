4b55d12b425e0e8310ad9ff0181fa07538158ef5
Sadab14 <sm.sadab.shakib@g.bracu.ac.bd>
2025-01-06 07:45:11 +0600

    eployee css changes

--- employee.php
+++ employee.php
@@ -37,7 +37,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Employee View</title>
-    <link rel="stylesheet" href="css/employee_style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/employee_styles.css">
 </head>
 <body>
     <header>
